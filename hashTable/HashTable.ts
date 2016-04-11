b2a1773e1d32204240dc7b8858824191f2179cd8
TheLox95 <leo_9_t@hotmail.com>
2016-04-11 16:35:01 -0430

    Implementacion completa de una tabla hash. No se si arrojar excepciones para el manejo de los errores sea una buena practica...

--- hashTable/HashTable.ts
+++ hashTable/HashTable.ts
@@ -9,24 +9,102 @@ interface HashEntry<KeyT, ValueT> {
 }
 
 export class HashTable<KeyT,ValueT>{
-	private _size;
+	private _modulo = 19;
 	private _count;
 	private _table: Array<SLL.LinkedList.SimpleList<HashEntryType<KeyT,ValueT>>>;
 	private _hashFunction: hashFunction;
 
-	constructor(func:hashFunction){
+	constructor(func: hashFunction = HashTable.getSumAllCharCode) {
 		this._hashFunction = func;
 		this._count = 0;
 		this._table = [];
 	}
 
-	public insert(key:KeyT, value:ValueT){
+	private static getSumAllCharCode(word:string){
+		let lengthWord = word.length;
+		let sum = 0;
+		for (var i = 0; i < lengthWord; i++) {
+			sum += word.charCodeAt(i);
+		}
+
+		return sum;
+	}
+
+	private calculateHash(key: KeyT) {
+		return this._hashFunction(key) % this._modulo;
+	}
+
+	private existsIndex(index:number){
+		if (this._table[index] == undefined) {
+			return false;
+		}
+		return true;
+	}
+
+	private increaceCount(){
+		this._count++;
+	}
+
+	private decreaseCount(){
+		this._count--;
+	}
+
+	public insert(key:KeyT, value:ValueT):this{
 		var entry: HashEntryType<KeyT, ValueT> = {
 			value : value,
 			key : key
 		};
-		var index: number = this._hashFunction(key);
-		console.log(index);
+
+		var index = this.calculateHash(key);
+
+		if (this.existsIndex(index) == false) {
+			this._table[index] = new SLL.LinkedList.SimpleList <HashEntryType<KeyT,ValueT>>();
+		}
+
+		this._table[index].append(entry);
+		this.increaceCount();
+
+		return this;
+	}
+
+	private ifIndexDoNotExistThrowErr(index){
+		if(this.existsIndex(index) == false) {
+			throw "This hash does't exists.";
+		}
+	}
+
+	public find(key:KeyT){
+		var index = this.calculateHash(key);
+
+		this.ifIndexDoNotExistThrowErr(index);
+
+		let iterador = this._table[index].getIterator();
+
+		while(iterador.isValid()){
+			if(iterador.getItem().key == key) {
+				return iterador.getItem().value;
+			}
+			iterador.forth();
+		}
+
+		throw "This element does't exist in the table.";
+	}
+
+	public remove(key: KeyT) {
+		var index = this.calculateHash(key);
+
+		this.ifIndexDoNotExistThrowErr(index);
+
+		let iterador = this._table[index].getIterator();
+
+		while (iterador.isValid()) {
+			if (iterador.getItem().key == key) {
+				this._table[index].remove(iterador);
+			}
+			iterador.forth();
+		}
+
+		this.decreaseCount();
 	}
 
 }
--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
 import ht = require("./hashTable/HashTable");
 
-var table = new ht.HashTable<string, string>((value) => { return 1 });
-table.insert("thelox95","leonardo");
+var table = new ht.HashTable<string, string>();
+table.insert("lo","Yo en alta definicion");
+table.insert("dd","Yo en 4K");
+
+ try{
+	console.log(table.find("lo"));
+	console.log(table.find("dd"));
+	console.log(table.find("ddaa"));
+}catch(err){
+	console.log(err);
+}
