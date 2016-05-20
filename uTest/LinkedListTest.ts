35ed369be1e683542f20df316054ec25c716a7a9
TheLox95 <leo_9_t@hotmail.com>
2016-05-20 10:47:11 -0430

    Agregadas pruebas unitarias para la clase LinkedList. Agregados a git el archivo de definicion de Typescript para Jasmine y el archivo de configuracion

---
+++ uTest/LinkedListTest.ts
@@ -0,0 +1,55 @@
+/// <reference path="../spec/support/jasmine.d.ts" />
+import {SimpleList,SimpleListIterator} from "../linkedList/LinkedList";
+
+describe("Linked List", () => {
+	let list = new SimpleList<string>();
+	let iterator: SimpleListIterator<string>;
+
+	it("should append elements to the list", () => {
+		expect(list.append("Iron Man")).toEqual(list);
+		expect(list.append("War Machine")).toEqual(list);
+		expect(list.append("Capitan America")).toEqual(list);
+		expect(list.append("Winter Soldier")).toEqual(list);
+		expect(list.append("Falcon")).toEqual(list);
+	});
+
+	it("should return the iterator of the list",()=>{
+		iterator = list.getIterator();
+		expect(iterator).toEqual(list.getIterator());
+	});
+
+	it("should go to the 2 nodes after of the list",()=>{
+		iterator	.forth()
+			.forth();
+
+		expect(iterator.getItem()).toBe("Capitan America");
+	});
+
+	it("should go to the first node of the list",()=>{
+		iterator.start();
+		expect(iterator.getItem()).toBe("Iron Man");
+	});
+
+	it("should remove the head node and set the head node to the next node",()=>{
+		list.removeHead();
+		iterator = list.getIterator();
+		expect(iterator.getItem()).toBe("War Machine");
+	});
+
+
+	it("should remove a node from the list and set nextNode to the next node",()=>{
+		expect(list.remove(iterator)).toEqual(list);
+		iterator = list.getIterator();
+		expect(iterator.getItem()).toBe("Capitan America");
+	});
+
+
+	it("should remove the tail of the list and set last node to the previous node",()=>{
+		list.removeTail();
+		expect(list.getTail().getValue()).toBe("Winter Soldier");
+	});
+
+	it("should go to the value of the main node",()=>{
+		expect(list.getIterator().getItem()).toBe("Capitan America");
+	});
+});
