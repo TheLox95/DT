0c9e920b12830621ad135fc6ca6ccfea6794b413
TheLox95 <leo_9_t@hotmail.com>
2016-05-25 11:46:01 -0430

    Modificado el nombre del autor. Agregado el archivo main.ts para pruebas.

---
+++ main.ts
@@ -0,0 +1,21 @@
+import {Heap} from "./heap/Heap";
+import {Queue} from "./queue/Queue"
+import {BinaryTree} from "./tree/BinaryTree"
+import {Tree} from "./tree/Tree"
+import {Graph} from "./graph/Graph"
+import {Stack} from "./stack/Stack"
+import {HashTable} from "./hashTable/HashTable"
+import {SimpleList, Iterator} from "./linkedList/LinkedList"
+import {DoubleLinkedList,DoubleListIterator} from "./linkedList/DoubleLinkedList"
+
+export {
+	Heap,
+	Queue,
+	BinaryTree,
+	Tree,
+	Graph,
+	Stack,
+	HashTable,
+	SimpleList as LinkedList, Iterator as LinkedListIterator,
+	DoubleLinkedList, DoubleListIterator
+};
